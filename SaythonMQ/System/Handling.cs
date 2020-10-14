f6ad6004fd83ca95cdc535b6f4d7db1bb370edf7
maurana <maurana.hasegawa@gmail.com>
2020-10-15 02:56:18 +0700

    Development System

---
+++ SaythonMQ/System/Handling.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SaythonMQ.System
+{
+    public static class Handling
+    {
+        public static IServiceCollection AddEventBusHandling<T>(this IServiceCollection services, IEnumerable<T> handlers)
+        {
+            foreach (var handler in handlers)
+            {
+                services.AddTransient(handler.GetType());
+            }
+
+            return services;
+        }
+    }
+}
---
+++ SaythonMQ/System/IDynamicIntegrationEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace SaythonMQ.System
+{
+    public interface IDynamicIntegrationEventHandler
+    {
+        Task Handle(dynamic eventData);
+    }
+}
